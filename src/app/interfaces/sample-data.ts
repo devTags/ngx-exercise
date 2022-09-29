cfb66df1acdb693e1f3907b50cf2d643092476fa
Gerald Tagle <78480062+devTags@users.noreply.github.com>
2022-09-29 14:56:01 +0800

    Sample-data for charts

---
+++ src/app/interfaces/sample-data.ts
@@ -0,0 +1,146 @@
+export const UnemploymentInfoData = [
+  {
+    countryId: '108ddbf6-b2e4-4305-aad8-149b0050ad2e',
+    countryName: 'Singapore           ',
+    unemploymentRate: 2.1,
+    recordedYear: '2022-06-20'
+  },
+  {
+    countryId: '96303191-a532-420b-879d-3df21abc90cc',
+    countryName: 'Thailand            ',
+    unemploymentRate: 2.0,
+    recordedYear: '2022-06-20'
+  },
+  {
+    countryId: '9f6e9a75-8973-4eb5-b42b-3e82a4947b74',
+    countryName: 'Hong Kong           ',
+    unemploymentRate: 4.1,
+    recordedYear: '2022-08-20'
+  },
+  {
+    countryId: '33b51fd9-aec5-4954-b89e-507a87afa26d',
+    countryName: 'China               ',
+    unemploymentRate: 5.3,
+    recordedYear: '2022-08-20'
+  },
+  {
+    countryId: 'a2eca057-ae65-467d-8b68-7f025a00751f',
+    countryName: 'Vietnam             ',
+    unemploymentRate: 2.46,
+    recordedYear: '2022-03-20'
+  },
+  {
+    countryId: '109d8723-c023-486a-8e0e-847466a25a8b',
+    countryName: 'South Korea         ',
+    unemploymentRate: 2.5,
+    recordedYear: '2022-08-20'
+  },
+  {
+    countryId: '177d32f6-78e6-4b5b-b8fb-890a342b5ed3',
+    countryName: 'Malaysia            ',
+    unemploymentRate: 2.7,
+    recordedYear: '2022-07-20'
+  },
+  {
+    countryId: 'e5baf9fd-b64b-4821-ba3e-ab018f822746',
+    countryName: 'Taiwan              ',
+    unemploymentRate: 3.6,
+    recordedYear: '2022-08-20'
+  },
+  {
+    countryId: 'd4796be3-290c-464c-8a55-d4d5dc244441',
+    countryName: 'Japan               ',
+    unemploymentRate: 2.6,
+    recordedYear: '2022-07-20'
+  },
+  {
+    countryId: 'bb886119-7d6c-481f-816d-e4637115537b',
+    countryName: 'Philippines         ',
+    unemploymentRate: 5.2,
+    recordedYear: '2022-06-20'
+  },
+  {
+    countryId: 'b54eff0e-83fc-48af-9bcf-f1a0306fd203',
+    countryName: 'Indonesia           ',
+    unemploymentRate: 5.83,
+    recordedYear: '2022-03-20'
+  }
+]
+
+export const SaleInfoData = [
+  {
+    saleId: '6f7a8c38-5089-4958-9071-7a0c1e299858',
+    saleRate: 8.0,
+    saleDate: '2009      '
+  },
+  {
+    saleId: '03818a92-df72-4bd5-abfc-e2d249aba250',
+    saleRate: 7.2,
+    saleDate: '2010      '
+  },
+  {
+    saleId: '5ef3a8e8-d24f-49a6-b5b6-b53fc7297be9',
+    saleRate: 4.5,
+    saleDate: '2011      '
+  },
+  {
+    saleId: 'a81548e8-f10b-4cbe-90cb-a54b23eee7e9',
+    saleRate: 5.8,
+    saleDate: '2012      '
+  },
+  {
+    saleId: 'e3406e00-5e98-4dd1-b0ec-a2008c336349',
+    saleRate: 10.0,
+    saleDate: '2013      '
+  },
+  {
+    saleId: '18341d61-e777-4f18-ad03-a0d233ea1ab7',
+    saleRate: 15.5,
+    saleDate: '2014      '
+  },
+  {
+    saleId: '7e409681-c322-4e8a-ba80-84e35e59ce05',
+    saleRate: 8.6,
+    saleDate: '2015      '
+  },
+  {
+    saleId: '65eb0f28-b598-4940-bf77-65697f2940cd',
+    saleRate: 2.5,
+    saleDate: '2016      '
+  },
+  {
+    saleId: '7d89a6cc-1918-4d23-ac43-54f6b40b4096',
+    saleRate: 1.5,
+    saleDate: '2017      '
+  },
+  {
+    saleId: '8d52552d-1df9-4aab-93ec-4dc21b34153a',
+    saleRate: 2.0,
+    saleDate: '2018      '
+  },
+  {
+    saleId: '1834c5ac-b092-470f-add1-298f8cb48028',
+    saleRate: 0.5,
+    saleDate: '2019      '
+  },
+  {
+    saleId: 'c0df7140-0764-4122-a6f0-16b58bcfddf5',
+    saleRate: 12.5,
+    saleDate: '2020      '
+  },
+  {
+    saleId: '9ba631a1-24d9-48db-ae33-1163d9e77c43',
+    saleRate: 9.5,
+    saleDate: '2021      '
+  }
+]
+
+export const WorldPopulationData = [
+  { name: 'Philippines', value: 110000000 },
+
+  { name: 'China', value: 1400000000 },
+
+  { name: 'USA', value: 329000000 },
+
+  { name: 'Japan', value: 125000000 }
+]
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,6 +5,11 @@ import { UnemploymentInfo } from '../../interfaces/unemployment-info'
 import { DataService } from '../../services/data.service'
 import { Color, ScaleType } from '@swimlane/ngx-charts'
 import { ngxChartData } from 'src/app/interfaces/ngx-data'
+import {
+  SaleInfoData,
+  UnemploymentInfoData,
+  WorldPopulationData
+} from 'src/app/interfaces/sample-data'
 
 @Component({
   selector: 'app-dashboard',
@@ -12,9 +17,10 @@ import { ngxChartData } from 'src/app/interfaces/ngx-data'
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  public chartData: UnemploymentInfo[] = []
-  public chartData1: SaleInfo[] = []
-  public ngxChartData: ngxChartData[] = []
+  public chartData: UnemploymentInfo[] = UnemploymentInfoData
+  public chartData1: SaleInfo[] = SaleInfoData
+  public ngxChartData: ngxChartData[] = WorldPopulationData
+
   public primaryXAxis!: Object
   public primaryYAxis!: Object
 
@@ -47,48 +53,52 @@ export class DashboardComponent implements OnInit {
   constructor (private ds: DataService) {}
 
   ngOnInit (): void {
-    this.getSales()
-    this.getAllUnemployed()
-    this.getPopulation()
-  }
-
-  getAllUnemployed (): void {
-    this.ds.getAllUnemploymentData().subscribe((res: UnemploymentInfo[]) => {
-      this.chartData = res
-    })
     this.primaryXAxis = {
       valueType: 'Category'
     }
     this.marker = { visible: true }
+    // this.getSales()
+    // this.getAllUnemployed()
+    // this.getPopulation()
   }
 
-  getSales (): void {
-    this.ds.getAllSales().subscribe((res: SaleInfo[]) => {
-      this.chartData1 = res
-      this.primaryXAxis1 = {
-        title: 'Year',
-        minimum: 2010,
-        maximum: new Date().getFullYear() - 1,
-        interval: 1,
-        edgeLabelPlacement: 'Shift'
-      }
-      this.primaryYAxis1 = {
-        minimum: 0,
-        maximum: 16,
-        interval: 2,
-        title: 'Sales Amount in Millions'
-      }
-    })
-  }
+  // getAllUnemployed (): void {
+  //   this.ds.getAllUnemploymentData().subscribe((res: UnemploymentInfo[]) => {
+  //     this.chartData = res
+  //   })
+  //   this.primaryXAxis = {
+  //     valueType: 'Category'
+  //   }
+  //   this.marker = { visible: true }
+  // }
 
-  getPopulation (): void {
-    this.ds.getWorldPopulation().subscribe((res: WorldPopulation[]) => {
-      this.ngxChartData = res.map(value => ({
-        name: value['countryName'],
-        value: Number(value['countryPopulation'])
-      }))
-    })
-  }
+  // getSales (): void {
+  //   this.ds.getAllSales().subscribe((res: SaleInfo[]) => {
+  //     this.chartData1 = res
+  //     this.primaryXAxis1 = {
+  //       title: 'Year',
+  //       minimum: 2010,
+  //       maximum: new Date().getFullYear() - 1,
+  //       interval: 1,
+  //       edgeLabelPlacement: 'Shift'
+  //     }
+  //     this.primaryYAxis1 = {
+  //       minimum: 0,
+  //       maximum: 16,
+  //       interval: 2,
+  //       title: 'Sales Amount in Millions'
+  //     }
+  //   })
+  // }
+
+  // getPopulation (): void {
+  //   this.ds.getWorldPopulation().subscribe((res: WorldPopulation[]) => {
+  //     this.ngxChartData = res.map(value => ({
+  //       name: value['countryName'],
+  //       value: Number(value['countryPopulation'])
+  //     }))
+  //   })
+  // }
 
   onSelect (event: any): void {
     console.log(event)
